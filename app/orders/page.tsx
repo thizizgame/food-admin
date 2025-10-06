f00f489b057a12f870d7a0a625ca66fe7812bda3
25LP4949 25LP4949 <25LP4949@G301-CC0194.local>
2025-10-06 12:01:05 +0800

    message

---
+++ app/orders/page.tsx
@@ -0,0 +1,9 @@
+export const OrdersPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
---
+++ app/products/page.tsx
@@ -0,0 +1,9 @@
+export const ProductsPage = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+
